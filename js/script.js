a8b5e0c13fd5760d59c78f6cd3703226deebf03d
michael <mikelkarije@gmail.com>
2019-07-19 16:33:03 +0300

    add images to the page

---
+++ js/script.js
@@ -0,0 +1,6 @@
+<div class="container-fluid">
+            <p>SERVICES</p>
+            <p>Before we sign a contract or write a fine code our team willspend the necessary time needed to understand your product vision.</p>
+            <p>The outcome of this scoping session is proposal or high-level statement of work.</p>
+            <p>based on your requirements,we can provide acomplete offering or just the pieces you need.</p>
+        </div>
